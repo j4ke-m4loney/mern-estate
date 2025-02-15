1c6b5b755b5ddd6f36c2d5b25b316ca412b45a4f
jakemaloney <jakemal85@gmail.com>
2025-02-15 13:59:39 +1100

    Fix: Add client folder as a normal directory

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+// src/pages/Home.jsx
+import React from 'react';
+
+export default function Home() {
+
+  return (
+    <div>
+      <h1>Home, welcome!</h1>
+    </div>
+  );
+}
---
+++ client/vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // ✅ Alias for `src/`
+    },
+  },
+});
