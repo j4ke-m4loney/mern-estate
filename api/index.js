2708e394a41b67c087da21d0df43cbdf1c02413d
jakemaloney <jakemal85@gmail.com>
2025-02-17 12:37:15 +1100

    adding signin

--- api/index.js
+++ api/index.js
@@ -4,16 +4,16 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 
-
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then( () => {
-  console.log("Connected to MongoDB");
-}).catch( (err) => {
-  console.log("Error: " + err);
-})
-
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error: " + err);
+  });
 
 const app = express();
 app.use(express.json());
@@ -22,15 +22,16 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000!!!");
 });
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
+// Centralized error handing  
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "internal Server Error"
-  return res.status(statusCode).json({
+  const statusCode = err.statusCode || 500; // Default to 500 if no status code is provided
+  const message = err.message || "internal Server Error"; // Default error message
+  return res.status(statusCode).json({ // Send a JSON response with error details
     success: false,
-    statusCode, 
-    message
+    statusCode,
+    message,
   });
-})
+});
